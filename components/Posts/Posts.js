8f9aab157e7f4dab964fd8935a7452793ac112a1
Jan Zunec <zunec.jan@gmail.com>
2022-03-02 10:18:44 +0100

    Fetched data from the API

--- components/Posts/Posts.js
+++ components/Posts/Posts.js
@@ -1,11 +1,31 @@
 import { StyleSheet, Text, View, Linking } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../NavBar/NavBar';
+import Post from './Post/Post';
 
 export default function Blog() {
+	const [data, setData] = useState([]);
+
+	useEffect(async () => {
+		const resp = await fetch('https://graphqlzero.almansi.me/api', {
+			method: 'POST',
+			headers: { 'content-type': 'application/json' },
+			body: JSON.stringify({
+				query: `{posts {data {id, title, body, user{id ,name, username}}}}`,
+			}),
+		});
+		const fetchedData = resp.json().then((res) => console.log(res));
+
+		console.log(fetchedData);
+
+		// const sortedData = fetchedData.then(resp => )
+		setData(fetchedData);
+	}, []);
+
 	const openLink = () => {
 		Linking.openURL('https://covstats-19.pages.dev');
 	};
+
 	return (
 		<View>
 			<Text style={styles.mainText}>
@@ -14,6 +34,7 @@ export default function Blog() {
 					Covstats-19
 				</Text>
 			</Text>
+			<Post />
 			<View>
 				<NavBar />
 			</View>
